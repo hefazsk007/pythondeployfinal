04261cc6e229f8a541a0d80a16879f4ae673cf02
root <root@ip-172-31-44-3.ap-south-1.compute.internal>
2021-06-26 11:04:45 +0000

    Python commit

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
 from flask import Flask
-app=Flask(__name__)
-@app.route("/")
-def homepage():
-    return "Hi Bharadwaj"
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
+
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Why Hello %s!\n' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)     # open for everyone
+
